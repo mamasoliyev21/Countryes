51dc7ee8ddb6f7cc55dc57d372ef8525f3fd7482
mamasoliyev21 <mamasoliyevislom4@gmail.com>
2022-11-27 12:08:01 +0500

    end country

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 "use strict"  
-const URl = "https://restcountries.com/v2/"
+const BaseUrl = "https://restcountries.com/v2/all"
+const Url = 'https://restcountries.com/v2/'
 //ALL COUNTRYES///
-const  getCountry  =async()=>{
-    const countryes =await fetch(`${URl}/all`)
+
+const  getCountry = async()=>{
+    const countryes =await fetch(`${BaseUrl}`)
     const result = await countryes.json()
     dataRender(result)
     dinamicCatagory(result)
@@ -14,7 +16,9 @@ getCountry()
 
 
 function dataRender (data=[]){
+    // console.log(data);
     data.forEach((el)=>{
+        // console.log(el);
         const card = document.createElement("div")
         card.classList.add("shadow")
         card.innerHTML=`
@@ -28,11 +32,13 @@ function dataRender (data=[]){
         <li class="card-list-item list-unstyled"><strong class='str'>population: ${el.population}</strong></li>
         <li class="card-list-item list-unstyled"><strong class='str'>region: ${el.region}</strong></li>
         <li class="card-list-item list-unstyled"><strong class='str'>capital:  ${el.capital}</strong></li>
-        <li class="card-list-item list-unstyled"><strong class='str'>independent:  ${el.independent}</strong></li>
+        <li class="card-list-item list-unstyled"><strong class='str'>independent: true</strong></li>
         </ul>
+        <button class="btn btn-primary" type="button" data-set="${el.area}">Read more</button>
         </div>
         </div>
         `
+        card.dataset.id=el.name
         
         $(".wrapper").appendChild(card)
     })
@@ -60,71 +66,173 @@ function dinamicCatagory (data){
     })
 }
 
+
 //end//
 
 
 
-$("#search").addEventListener("keyup",(e)=>{
-    $(".wrapper").innerHTML =""
-    $(".info").innerHTML=`<div class="spinner">  
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    </div>`
+$("#search").addEventListener("keypress",(e)=>{
+    $(".wrapper").innerHTML ='<div class="lds-heart"><div></div></div>'
     setTimeout(()=>{
         if(e.target.value.trim().length!==0 && e.keyCode===13){ 
-            Findcountry(e.target.value)
+            findCountry(e.target.value)
         }
     },500)
 })
 
 // search function//
 
-async function Findcountry(country){
-    $(".wrapper").innerHTML=""
-    const restponse = await fetch(`${URl}/name/${country}`)
-    const countr = await restponse.json()
-    if(restponse.status ===404){
-        $(".info").innerHTML=`<h1 class='text-center'>Bunday davlat yoq</h1>`
-    } else{
-        dataRender(countr)
-        $(".info").innerHTML = `<h3 class='text-center'>${countr.length} davlat topildi</h3>`
-    }
-}
-//end search//
 
-$("#region").addEventListener("change",(e)=>{
+async function findCountry(country) {
+    $(".wrapper").innerHTML = "";
+    const response = await fetch(`${Url}/name/${country}`);
+    const data = await response.json();
+    if (response.status === 404) {
+        $(".info").innerHTML =
+        "<h1 class='text-center text-else'>BUNDAY DAVLAT YO'Q</h1>";
+    } else {
+        $(
+            ".info"
+            ).innerHTML = `<h1 class='text-center text-else w-100'>Qidiruv natijasi: ${data.length}</h1>`;
+            dataRender(data);
+        }
+    }
+    
+    
+    
+    
+    
+    $("#region").addEventListener("change",(e)=>{
         sortcountry(e.target.value.toLowerCase())    
-})
-
-async function sortcountry(region){
-    $(".wrapper").innerHTML=""
-    if(region==="all"){
-        const restponse = await fetch(`${URl}/all`)
-        const countr = await restponse.json()
-        if(restponse.status ===404){
-            $(".info").innerHTML=`<h1 class='text-center'>Not found/h1>`
+    })
+    
+    async function sortcountry(region){
+        $(".wrapper").innerHTML=""
+        if(region==="all"){
+            const restponse = await fetch(`${BaseUrl}`)
+            const countr = await restponse.json();
+            console.log(countr);
+            if(restponse.status ===404){
+                $(".info").innerHTML=`<h1 class='text-center'>Not found/h1>`
+            } else{
+                dataRender(countr)
+                $(".info").innerHTML = `<h3 class='text-center'>${countr.length} davlat topildi</h3>`
+            }
         } else{
-            dataRender(countr)
-            $(".info").innerHTML = `<h3 class='text-center'>${countr.length} davlat topildi</h3>`
-        }
-    } else{
-        const restponse = await fetch(`${URl}/region/${region}`)
-        const countr = await restponse.json()
-        if(restponse.status ===404){
-            $(".info").innerHTML=`<h1 class='text-center'>Not found/h1>`
-        } else{
-            dataRender(countr)
-            $(".info").innerHTML = `<h3 class='text-center'>${countr.length} davlat topildi</h3>`
+            const restponse = await fetch(`${Url}/region/${region}`)
+            const countr = await restponse.json()
+            if(restponse.status ===404){
+                $(".info").innerHTML=`<h1 class='text-center'>Not found/h1>`
+            } else{
+                dataRender(countr)
+                $(".info").innerHTML = `<h3 class='text-center'>${countr.length} davlat topildi</h3>`
+            }
         }
     }
-}
+    
+    /// end/
+    
+    $(".wrapper").addEventListener("click",(e)=>{
+        $(".country-info").innerHTML =""
+        if(e.target.classList.contains("btn-primary")){
+            let id = e.target.getAttribute("data-set");        
+            $(".sidebar").classList.remove("swipe");
+            $(".sidebar").classList.add("pos");
+            $("body").style.overflow= "hidden";
+            getcount(id)
+        }
+    })
+    
+    
+    
+    async function getcount(country){
+        const response=await fetch(`${BaseUrl}`)
+        const result = await response.json();
+        console.log(result);
+        const data = result.filter(item => {
+            return item.area == country;
+        });
+        
+        data.forEach(item => {
+            console.log(item)
+            const creat = createElement("div","row w-100",`
+            <div class="col-md-4 ps-5  easy">
+            <img class="lef " src="${item.flags.svg}" alt="">
+            <div class="col-md-2 w-50 ps-5 ul-list ">
+            <ul class="list-group w-100   ">
+            <li class="list-group-item w-100"><strong>Country: ${item.name}</strong></li>
+            <li class="list-group-item w-100"><strong>Population:${item.population}</strong></li>
+            <li class="list-group-item w-100"> <strong>Region:   ${item.region}</strong></li>
+            <li class="list-group-item w-100"> <strong>Region:   ${item.subregion}</strong></li>
+            <li class="list-group-item w-100"><strong>Capital: ${item.capital}</strong></li>
+            <li class="list-group-item w-100"><strong>: demonym ${item.demonym}</strong></li>
+            <li class="list-group-item w-100"><strong>language: ${item.independent}</strong></li>
+            
+            
+            
+            </ul>
+            </div>
+            </div>
+            `);
+            $(".country-info").appendChild(creat);
+        });
+        
+    }
+    
+    $(".close").addEventListener("click", (e)=>{
+        $(".country-info").innerHTML = ""
+        $(".sidebar").classList.add("swipe")
+        $(".sidebar").classList.add("pos")
+        $("body").style.overflow= "visible";
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
